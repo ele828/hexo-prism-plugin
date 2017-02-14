3040f9572ec0431d1022e89b94365fd3be64e267
Sabibullah <620378@CTS.COM>
2017-02-14 11:59:45 +0530

    fix: Support for all languages; Injecting scripts and stylesheets in head tag instead of body

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 
 const fs = require('hexo-fs');
 const path = require('path');
-const Prism = require('prismjs');
+const Prism = require('node-prismjs');
 require('prismjs/plugins/line-numbers/prism-line-numbers.min');
 
 const map = {
@@ -15,6 +15,19 @@ const map = {
 
 const regex = /<pre><code class="(.*)?">([\s\S]*?)<\/code><\/pre>/igm;
 
+const baseDir = hexo.base_dir;
+const prismDir = path.join(baseDir, 'node_modules', 'prismjs');
+const prismThemeDir = path.join(prismDir, 'themes');
+const prismjsFilePath = path.join(prismThemeDir, 'prism.js');
+
+// Plugin settings from config
+let prismThemeName = hexo.config.prism_plugin.theme || '';
+let mode = hexo.config.prism_plugin.mode || 'preprocess';
+let line_number = hexo.config.prism_plugin.line_number || false;
+
+const prismThemeFileName = 'prism' + (prismThemeName === 'default' ? '' : `-${prismThemeName}`) + '.css';
+const prismThemeFilePath = path.join(prismThemeDir, prismThemeFileName);
+
 // Unescape from Marked escape.
 function unescape(str) {
   if (!str || str == null) return '';
@@ -22,49 +35,7 @@ function unescape(str) {
   return String(str).replace(re, (match) => map[match]);
 };
 
-const baseDir = hexo.base_dir;
-const pluginDir = path.join(
-    baseDir, 'node_modules', 'hexo-prism-plugin');
-const prismDir = path.join(
-    baseDir, 'node_modules', 'prismjs');
-const themeDir = path.join(prismDir, 'themes');
-const libUri = path.join(prismDir, 'prism.js');
-
-// Process sub-directory
-let configRoot = hexo.config.root;
-if (configRoot && configRoot !== '/') {
-  if (configRoot[configRoot.length - 1] === '/')
-    configRoot = configRoot.slice(0, configRoot.length - 1);
-} else
-  configRoot = '';
-
 function PrismPlugin(data) {
-  let theme = hexo.config.prism_plugin.theme  || '';
-  let mode = hexo.config.prism_plugin.mode || 'preprocess';
-  let line_number = hexo.config.prism_plugin.line_number || false;
-
-  // Copy script and stylesheet files
-  if (theme === 'default') theme = '';
-  else {
-    theme = '-' + theme;
-  }
-  const themeFile = 'prism' + theme + '.css';
-  const themeUri = path.join(themeDir, themeFile);
-  fs.copyFile(themeUri, path.join(baseDir, 'public', 'css', themeFile));
-
-  if (mode === 'realtime') {
-    fs.copyFile(path.join(prismDir, 'prism.js'),
-      path.join(baseDir, 'public', 'js', 'prism.js'));
-    if (line_number) {
-      fs.copyFile(path.join(prismDir, 'plugins/line-numbers', 'prism-line-numbers.min.js'),
-        path.join(baseDir, 'public', 'js', 'prism-line-numbers.min.js'));
-    }
-  }
-  
-  if (line_number) {
-    fs.copyFile(path.join(prismDir, 'plugins/line-numbers', 'prism-line-numbers.css'),
-      path.join(baseDir, 'public', 'css', 'prism-line-numbers.css'));
-  }
 
   data.content = data.content.replace(regex, (origin, lang, code) => {
     if (lang === 'obj-c') lang = 'objectivec';
@@ -73,11 +44,11 @@ function PrismPlugin(data) {
     const endTag = `</code></pre>`;
     code = unescape(code);
     let parsedCode = '';
-    if (Prism.languages[lang]) {      
+    if (Prism.languages[lang]) {
       parsedCode = Prism.highlight(code, Prism.languages[lang]);
     }
     else parsedCode = code;
-    if(line_number) {
+    if (line_number) {
       let match = parsedCode.match(/\n(?!$)/g);
       let linesNum = match ? match.length + 1 : 1;
       let lines = new Array(linesNum + 1);
@@ -89,20 +60,60 @@ function PrismPlugin(data) {
     return startTag + parsedCode + endTag;
   });
 
-  // Inject script and stylesheet to post pages
-  let jsImports = '';
-  if (mode === 'realtime') {
-    jsImports = `<script src="${configRoot}/js/prism.js"></script>`;
-    if (line_number) {
-      jsImports += `<script src="${configRoot}/js/prism-line-numbers.min.js"></script>`;
-    }
-  }
-  let cssImports = `<link href="${configRoot}/css/${themeFile}" rel="stylesheet">`;
-  if (line_number) {
-    cssImports += `<link href="${configRoot}/css/prism-line-numbers.css" rel="stylesheet">`;
-  }
-  data.content += cssImports + jsImports;
   return data;
 }
 
 hexo.extend.filter.register('after_post_render', PrismPlugin);
+
+hexo.extend.generator.register('prism_assets', function () {
+
+  // Register scripts and stylesheets
+  let assets = [{
+    path: `css/${prismThemeFileName}`,
+    data: () => fs.createReadStream(prismThemeFilePath)
+  }];
+  // If line_number is enabled in plugin config add the corresponding stylesheet
+  if(line_number) {
+    assets.push({
+      path: 'css/prism-line-numbers.css',
+      data: () => fs.createReadStream(path.join(prismDir, 'plugins/line-numbers', 'prism-line-numbers.css'))
+    });
+  }
+  // If prism plugin config mode is realtime include prism.js and line-numbers.js
+  if (mode === 'realtime') {
+    assets.push({
+      path: 'js/prism.js',
+      data: () => fs.createReadStream(path.join(prismDir, 'prism.js'))
+    });
+    if (line_number) {
+      assets.push({
+        path: 'js/prism-line-numbers.min.js',
+        data: () => fs.createReadStream(path.join(prismDir, 'plugins/line-numbers', 'prism-line-numbers.min.js'))
+      });
+    }
+  }
+
+  return assets;
+});
+
+hexo.extend.filter.register('after_render:html', function (str, data) {
+
+  let css = `<link rel="stylesheet" href="/css/${prismThemeFileName}" type="text/css">`;
+
+  let js = '';
+
+  if(line_number) {
+    css += `<link rel="stylesheet" href="/css/prism-line-numbers.css" type="text/css">`;
+  }
+
+  if(mode === 'realtime') {
+    js +=  '<script src="/js/prism.js"></script>';
+    if(line_number) {
+      js += '<script src="/js/prism-line-numbers.min.js"></script>';
+    }
+  }
+
+  str = str.replace(/<\s*\/\s*head\s*>/, css + js + '</head>');
+  //console.log('String ', str);
+  return str;
+});
